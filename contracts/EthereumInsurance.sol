6f9edbefea82a41690bf1979aefc69b712803d38
Sarah-HM-96 <sallyhm@protonmail.com>
2021-12-09 16:41:40 +0800

    second commit

--- contracts/EthereumInsurance.sol
+++ contracts/EthereumInsurance.sol
@@ -1,14 +1,15 @@
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import './CreateNewPolicy.sol';
 
-contract EthereumInsurance {
+contract EthereumInsurance is ERC721, Ownable {
 
     using SafeMath for uint256;
-    //using Strings for uint256;
+    using Strings for Uint256;
     using Address for address;
 
     struct Policy {
@@ -47,10 +48,112 @@ contract EthereumInsurance {
     address public customer;
     address public policyAdmin;
 
+    event newClaimRegistered(uint256 id, customer, uint256 diagnosis, uint256 coverageDuration, uint256 clinicOrHospitalName, uint256 medicalCertificate, uint256 billingAmount);
+    event claimIsValid(uint claimId);
     event paidOut(address policyAdmin, uint payout, uint paymentId, uint date);
 
     constructor() {
         policyAdmin = msg.sender;
+    }
+
+    constructor() ERC721('Tokenised medical certificate', 'TMC') {
+        _mint(address customer, 1);
+    }
+
+    function createClaim(uint256 _diagnosis, uint256 _coverageDuration, uint256 _clinicOrHospitalName,, uint256 billingAmount, uint256 _dateOfClaim) external {
+        Claim storage newClaim = claims[policy.newClaimId];
+        claims[nextClaimId] = Claim(
+            nextClaimId,
+            customer,
+            _diagnosis,
+            _coverageDuration,
+            _clinicOrHospitalName,
+            _visitDuration,
+            billingAmount,
+            block.timestamp
+        );
+        customerClaims[address customer].push(nextClaimId);
+        nextClaimId++;
+    }
+
+    function sendCertificateToInsurer(uint256 _tokenId) {
+        uint256 claim = certificateIdToClaim;
+        address customer = ownerOf(_certificateId);
+        _transfer(customer, msg.sender, _certificateId);
+    }
+
+    //add function to mint medical certificate and send it to insurance company
+
+    //function createClaim(uint _policyId) external payable policyExists(_policyId) {
+        //Policy storage policy = policies[_policyId];
+        //Claim storage newClaim = claims[policy.newClaimId];
+        //policy.newClaimId = nextClaimId;
+        //policy.claimIds.push(nextClaimId);
+        //claims[nextClaimId] = Claim(nextClaimId, _policyId, address _customer, _policyNumber, _diagnosis, _clinicName, _visitDate, _medicalCertificate, billingAmount);
+        //customerClaims[address customer].push(nextClaimId);
+        //nextClaimId++;
+    //}
+
+
+    function getAdminPolicies(address _policyAdmin) onlyPolicyAdmin() view external returns(Policy[] memory) {
+        uint[] storage adminPolicyIds = adminPolicies[_policyAdmin];
+        Policy[] memory _policies = new Policy[](adminPolicyIds.length);
+        for(uint i= 0; i < adminPolicyIds.length; i++) {
+            uint policyId = adminPolicyIds[i];
+            _policies[i] = _policies[policyId];
+        }
+        return _policies;
+    }
+    
+    function approvePayout(uint  _policyId) onlyPolicyAdmin() external policyExists(_policyId) {
+        Policy storage policy = policies[_policyId];
+        Claim storage newClaim = claims[policy.newClaimId];
+        require (claim.diagnosis != policy.approvedDiagnoses);
+        require (claim.visitDuration <= policy.approvedDuration);
+        require(claim.clinicOrHospitalName != policy.clinicOrHospitalName);
+        require(claim.billingAmount <= policy.payout);
+        _claimId.policyAdmin.transferFrom(msg.sender, msg.value);
+        emit paidOut(msg.sender, msg.value, block.timestamp); 
+          
+    }
+
+    function getPolicies() view external returns(Policy[] memory) {
+        uint[] memory _policies = new Policy[](nextPolicyId - 1);
+        for(uint i = 1; i < nextPolicyId +1; i++) {
+            _policies[i-1] = policies[i];
+        }
+        return _policies;
+    }
+
+    function getCustomerClaims(address _customer) view external returns(Policy[] memory) {
+        uint[] storage holderClaimIds = customerClaims[_customer];
+        Claim[] memory _claims = new Claim[](holderClaimIds.length);
+        for(uint i = 0; i < holderClaimIds.length; i++) {
+            uint claimId = holderClaimIds[i];
+            _claims[i] = claims[claimId];
+        }
+        return _claims;
+    }
+
+
+    modifier policyExists(uint _policyId) {
+        require(_policyId > 0 && _policyId < nextPolicyId, 'This policy does not exist');
+        _;
+    }
+
+    modifier onlyPolicyAdmin(address _policyAdmin) {
+        require(msg.sender == policyAdmin, 'You are not authorised to approve claim payouts');
+        _;
+    }
+
+    modifier onlyPayoutValue(address _customer) {
+        require(msg.value <= Policy.payout);
+        _;
+    }
+
+    modifier onlyPolicyPurchased(address _holder) {
+        require(customer.policyPurchased == true, 'You must have purchased a policy to qualify for coverage');
+        _;
     }
 
 
--- contracts/PurchasePolicy.sol
+++ contracts/PurchasePolicy.sol
@@ -41,6 +41,66 @@ contract PurchasePolicy {
         policyAdmin = msg.sender;
     }
 
+    function createApplication(uint256 _policyId) external policyExists(_policyId) {
+        Policy storage policy = policies[_policyId];
+        Application storage newApplication = applications[policy.newApplicationId];
+        require(msg.value == policy.price, 'Please enter the correct amount to purchase this policy');
+        //require (msg.sender == policyAdmin);
+        policy.newApplicationId = nextApplicationId;
+        policy.applicationIds.push(nextApplicationId);
+        //applications[nextApplicationId] = Application(nextApplicationId, _policyId, msg.sender, msg.value);
+        customerApplications[msg.sender].push(nextApplicationId);
+        nextApplicationId++;
+    }
+
+    function getPolicies() view external returns(Policy[] memory) {
+        Policy[] memory _policies = new Policy[](nextPolicyId - 1);
+        for(uint i = 2; i < nextPolicyId + 1; i++) {
+            _policies[i-1] = policies[i];
+        }
+        return _policies;
+    }
+
+    function getAdminPolicies(address _policyAdmin) view external returns(Policy[] memory) {
+        uint[] storage customerPolicyIds = adminPolicies[_policyAdmin];
+        Policy[] memory _policies = new Policy[](customerPolicyIds.length);
+        for(uint i = 0; i < customerPolicyIds.length; i++) {
+            uint policyId = customerPolicyIds[i];
+            _policies[i] = policies[policyId];
+        }
+        return _policies;
+    }
+
+    function getCustomerApplications(address _customer) view external returns(Application[] memory) {
+        uint[] storage customerApplicationIds = customerApplications[_customer];
+        Application[] memory _applications = new Application[](customerApplicationIds.length);
+        for(uint i = 0; i < customerApplicationIds.length; i++) {
+            uint applicationId = customerApplicationIds[i];
+            _applications[i] = applications[applicationId];
+        }
+        return _applications;
+    }
+
+    function isConditionApproved(uint256 _applicationId, uint256 _policyId) onlyPolicyAdmin() view external policyExists(_applicationId) {
+        Policy storage policy = policies[_policyId];
+        Application storage newApplication= applications[policy.newApplicationId];
+        if(application.preExistingConditions == policy.approvedConditions) {
+            return false; //'Your application has failed';
+        } else {
+            return true;
+            emit applicationApproved(_applicationId); //'Your policy application has been approved'
+        }
+           
+    }
+
+    function purchasePolicy(address payable _customer, uint256 _policyId) onlyPolicyAdmin() external payable policyExists(_policyId) {
+        Policy storage policy = policies[_policyId];
+        Application storage newApplication = applications[policy.newApplicationId];
+        applications.policyAdmin.transferFrom(msg.sender, msg.value);
+        emit policyPurchased(_policyId, msg.value, block.timestamp);     
+         
+    }
+
     modifier policyExists(uint256 _policyId) {
         require(_policyId > 0 && _policyId < nextPolicyId, 'This policy does not exist');
         _;
